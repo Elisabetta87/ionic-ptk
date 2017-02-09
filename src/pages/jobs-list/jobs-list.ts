f7744005d85d360aa1b80b938f72633b34e2e9fc
Elisabetta87 <elisa87g@gmail.com>
2017-02-09 16:12:46 +0000

    jobs created

--- src/pages/jobs-list/jobs-list.ts
+++ src/pages/jobs-list/jobs-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {SecureStorage} from "ionic-native/dist/es5/index";
+import { SecureStorage } from 'ionic-native/dist/es5/index';
+import { NavController } from 'ionic-angular/index';
 
 
 @Component({
@@ -11,59 +12,52 @@ export class JobsListPage {
 
   private jobsAvailable: boolean;
   private jobs: any;
-  private status: Array<string>;
-  private statusColor: string;
+  private status: string;
 
-  constructor() {
+  constructor(
+    public navCtrl: NavController
+  ) {
 
     this.jobsAvailable = true;
 
-    this.status = ['complete', 'past and not complete', 'current and not complete', 'future', 'unaccepted'];
-    this.statusColor = this.status[0] ? 'green' : '';
+    //this.status = ['complete', 'past and not complete', 'current and not complete', 'future', 'unaccepted'];
 
     this.jobs = [
       { id      : '1',
         dateTime: '6-02-17',
         address : '64, Streatham Vale, SW16 5TD',
         service : 'cleaning',
-        status  : this.status[0]},
+        status  : 'green'},
       { id      : '2',
         dateTime: '18-01-17',
         address : '60, Tooting Bec, SW16 5TD',
         service : 'delivery grocery',
-        status  : this.status[1]},
+        status  : 'red'},
       { id      : '3',
         dateTime: '8-03-17',
         address : '54, Streatham Hill, SW16',
         service : 'pick keys up',
-        status  : this.status[2]},
+        status  : 'orange'},
       { id      : '4',
         dateTime: '12-04-17',
         address : '12, Balham, SW17',
         service : 'delivery keys to the new guest',
-        status  : this.status[3]},
+        status  : 'yellow'},
       { id      : '5',
         dateTime: '8-03-17',
         address : '4, Oval, SW16',
         service : 'cleaning',
-        status  : this.status[4]}
+        status  : 'grey'}
     ];
-
-    if(this.status[0]) {
-     this.statusColor = 'green';
-     } else if(this.status[1]) {
-     this.statusColor = 'red';
-     } else if(this.status[2]) {
-     this.statusColor = 'orange';
-     } else if(this.status[3]) {
-     this.statusColor = 'yellow';
-     } else if(this.status[4]) {
-     this.statusColor = 'grey';
-     }
   }
 
   //this function needs to be called inside future function that gets API jobs list info
   //getJobs() {}
+
+  jobInfo() {
+    console.log('More Info Please');
+    //this.navCtrl.setRoot()
+  }
 
 
 
